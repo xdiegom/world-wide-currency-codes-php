2b6bbbf7cac32de791adbbeb548a71e001023628
Diego <xdiegom@users.noreply.github.com>
2017-02-21 17:51:34 -0600

    Add files via upload

---
+++ Currency_Codes.php
@@ -0,0 +1,670 @@
+<?php
+
+$currencies = [
+
+    [
+        'Name' => 'Albania Lek',
+        'Code' => 'ALL',
+        'unicodeHEX' => '&#x4c;&#x65;&#x6b;'
+    ],
+
+    [
+        'Name' => 'Afghanistan Afghani',
+        'Code' => 'AFN',
+        'unicodeHEX' => '&#x60b;'
+    ],
+
+    [
+        'Name' => 'Argentina Peso',
+        'Code' => 'ARS',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Aruba Guilder',
+        'Code' => 'AWG',
+        'unicodeHEX' => '&#x192;'
+    ],
+
+    [
+        'Name' => 'Australia Dollar',
+        'Code' => 'AUD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Azerbaijan New Manat',
+        'Code' => 'AZN',
+        'unicodeHEX' => '&#x43c;&#x430;&#x43d;'
+    ],
+
+    [
+        'Name' => 'Bahamas Dollar',
+        'Code' => 'BSD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Barbados Dollar',
+        'Code' => 'BBD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Belarus Ruble',
+        'Code' => 'BYN',
+        'unicodeHEX' => '&#x42;&#x72;'
+    ],
+
+    [
+        'Name' => 'Belize Dollar',
+        'Code' => 'BZD',
+        'unicodeHEX' => '&#x42;&#x5a;&#x24;'
+    ],
+
+    [
+        'Name' => 'Bermuda Dollar',
+        'Code' => 'BMD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Bolivia Bolíviano',
+        'Code' => 'BOB',
+        'unicodeHEX' => '&#x24;&#x62;'
+    ],
+
+    [
+        'Name' => 'Bosnia and Herzegovina Convertible Marka',
+        'Code' => 'BAM',
+        'unicodeHEX' => '&#x4b;&#x4d;'
+    ],
+
+    [
+        'Name' => 'Botswana Pula',
+        'Code' => 'BWP',
+        'unicodeHEX' => '&#x50;'
+    ],
+
+    [
+        'Name' => 'Bulgaria Lev',
+        'Code' => 'BGN',
+        'unicodeHEX' => '&#x43b;&#x432;'
+    ],
+
+    [
+        'Name' => 'Brazil Real',
+        'Code' => 'BRL',
+        'unicodeHEX' => '&#x52;&#x24;'
+    ],
+
+    [
+        'Name' => 'Brunei Darussalam Dollar',
+        'Code' => 'BND',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Cambodia Riel',
+        'Code' => 'KHR',
+        'unicodeHEX' => '&#x17db;'
+    ],
+
+    [
+        'Name' => 'Canada Dollar',
+        'Code' => 'CAD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Cayman Islands Dollar',
+        'Code' => 'KYD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Chile Peso',
+        'Code' => 'CLP',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'China Yuan Renminbi',
+        'Code' => 'CNY',
+        'unicodeHEX' => '&#xa5;'
+    ],
+
+    [
+        'Name' => 'Colombia Peso',
+        'Code' => 'COP',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Costa Rica Colon',
+        'Code' => 'CRC',
+        'unicodeHEX' => '&#x20a1;'
+    ],
+
+    [
+        'Name' => 'Croatia Kuna',
+        'Code' => 'HRK',
+        'unicodeHEX' => '&#x6b;&#x6e;'
+    ],
+
+    [
+        'Name' => 'Cuba Peso',
+        'Code' => 'CUP',
+        'unicodeHEX' => '&#x20b1;'
+    ],
+
+    [
+        'Name' => 'Czech Republic Koruna',
+        'Code' => 'CZK',
+        'unicodeHEX' => '&#x4b;&#x10d;'
+    ],
+
+    [
+        'Name' => 'Denmark Krone',
+        'Code' => 'DKK',
+        'unicodeHEX' => '&#x6b;&#x72;'
+    ],
+
+    [
+        'Name' => 'Dominican Republic Peso',
+        'Code' => 'DOP',
+        'unicodeHEX' => '&#x52;&#x44;&#x24;'
+    ],
+
+    [
+        'Name' => 'East Caribbean Dollar',
+        'Code' => 'XCD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Egypt Pound',
+        'Code' => 'EGP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'El Salvador',
+        'Code' => 'SVC',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Euro Member Countries',
+        'Code' => 'EUR',
+        'unicodeHEX' => '&#x20ac;'
+    ],
+
+    [
+        'Name' => 'Falkland Islands (Malvinas) Pound',
+        'Code' => 'FKP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Fiji Dollar',
+        'Code' => 'FJD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Ghana Cedi',
+        'Code' => 'GHS',
+        'unicodeHEX' => '&#xa2;'
+    ],
+
+    [
+        'Name' => 'Gibraltar Pound',
+        'Code' => 'GIP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Guatemala Quetzal',
+        'Code' => 'GTQ',
+        'unicodeHEX' => '&#x51;'
+    ],
+
+    [
+        'Name' => 'Guernsey Pound',
+        'Code' => 'GGP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Guyana Dollar',
+        'Code' => 'GYD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Honduras Lempira',
+        'Code' => 'HNL',
+        'unicodeHEX' => '&#x4c;'
+    ],
+
+    [
+        'Name' => 'Hong Kong Dollar',
+        'Code' => 'HKD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Hungary Forint',
+        'Code' => 'HUF',
+        'unicodeHEX' => '&#x46;&#x74;'
+    ],
+
+    [
+        'Name' => 'Iceland Krona',
+        'Code' => 'ISK',
+        'unicodeHEX' => '&#x6b;&#x72;'
+    ],
+
+    [
+        'Name' => 'India Rupee',
+        'Code' => 'INR',
+        'unicodeHEX' => '&#x;'
+    ],
+
+    [
+        'Name' => 'Indonesia Rupiah',
+        'Code' => 'IDR',
+        'unicodeHEX' => '&#x52;&#x70;'
+    ],
+
+    [
+        'Name' => 'Iran Rial',
+        'Code' => 'IRR',
+        'unicodeHEX' => '&#xfdfc;'
+    ],
+
+    [
+        'Name' => 'Isle of Man Pound',
+        'Code' => 'IMP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Israel Shekel',
+        'Code' => 'ILS',
+        'unicodeHEX' => '&#x20aa;'
+    ],
+
+    [
+        'Name' => 'Jamaica Dollar',
+        'Code' => 'JMD',
+        'unicodeHEX' => '&#x4a;&#x24;'
+    ],
+
+    [
+        'Name' => 'Japan Yen',
+        'Code' => 'JPY',
+        'unicodeHEX' => '&#xa5;'
+    ],
+
+    [
+        'Name' => 'Jersey Pound',
+        'Code' => 'JEP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Kazakhstan Tenge',
+        'Code' => 'KZT',
+        'unicodeHEX' => '&#x43b;&#x432;'
+    ],
+
+    [
+        'Name' => 'Korea (North) Won',
+        'Code' => 'KPW',
+        'unicodeHEX' => '&#x20a9;'
+    ],
+
+    [
+        'Name' => 'Korea (South) Won',
+        'Code' => 'KRW',
+        'unicodeHEX' => '&#x20a9;'
+    ],
+
+    [
+        'Name' => 'Kyrgyzstan Som',
+        'Code' => 'KGS',
+        'unicodeHEX' => '&#x43b;&#x432;'
+    ],
+
+    [
+        'Name' => 'Laos Kip',
+        'Code' => 'LAK',
+        'unicodeHEX' => '&#x20ad;'
+    ],
+
+    [
+        'Name' => 'Lebanon Pound',
+        'Code' => 'LBP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Liberia Dollar',
+        'Code' => 'LRD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Macedonia Denar',
+        'Code' => 'MKD',
+        'unicodeHEX' => '&#x434;&#x435;&#x43d;'
+    ],
+
+    [
+        'Name' => 'Malaysia Ringgit',
+        'Code' => 'MYR',
+        'unicodeHEX' => '&#x52;&#x4d;'
+    ],
+
+    [
+        'Name' => 'Mauritius Rupee',
+        'Code' => 'MUR',
+        'unicodeHEX' => '&#x20a8;'
+    ],
+
+    [
+        'Name' => 'Mexico Peso',
+        'Code' => 'MXN',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Mongolia Tughrik',
+        'Code' => 'MNT',
+        'unicodeHEX' => '&#x20ae;'
+    ],
+
+    [
+        'Name' => 'Mozambique Metical',
+        'Code' => 'MZN',
+        'unicodeHEX' => '&#x4d;&#x54;'
+    ],
+
+    [
+        'Name' => 'Namibia Dollar',
+        'Code' => 'NAD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Nepal Rupee',
+        'Code' => 'NPR',
+        'unicodeHEX' => '&#x20a8;'
+    ],
+
+    [
+        'Name' => 'Netherlands Antilles Guilder',
+        'Code' => 'ANG',
+        'unicodeHEX' => '&#x192;'
+    ],
+
+    [
+        'Name' => 'New Zealand Dollar',
+        'Code' => 'NZD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Nicaragua Cordoba',
+        'Code' => 'NIO',
+        'unicodeHEX' => '&#x43;&#x24;'
+    ],
+
+    [
+        'Name' => 'Nigeria Naira',
+        'Code' => 'NGN',
+        'unicodeHEX' => '&#x20a6;'
+    ],
+
+    [
+        'Name' => 'Korea (North) Won',
+        'Code' => 'KPW',
+        'unicodeHEX' => '&#x20a9;'
+    ],
+
+    [
+        'Name' => 'Norway Krone',
+        'Code' => 'NOK',
+        'unicodeHEX' => '&#x6b;&#x72;'
+    ],
+
+    [
+        'Name' => 'Oman Rial',
+        'Code' => 'OMR',
+        'unicodeHEX' => '&#xfdfc;'
+    ],
+
+    [
+        'Name' => 'Pakistan Rupee',
+        'Code' => 'PKR',
+        'unicodeHEX' => '&#x20a8;'
+    ],
+
+    [
+        'Name' => 'Panama Balboa',
+        'Code' => 'PAB',
+        'unicodeHEX' => '&#x42;&#x2f;&#x2e;'
+    ],
+
+    [
+        'Name' => 'Paraguay Guarani',
+        'Code' => 'PYG',
+        'unicodeHEX' => '&#x47;&#x73;'
+    ],
+
+    [
+        'Name' => 'Peru Sol',
+        'Code' => 'PEN',
+        'unicodeHEX' => '&#x53;&#x2f;&#x2e;'
+    ],
+
+    [
+        'Name' => 'Philippines Peso',
+        'Code' => 'PHP',
+        'unicodeHEX' => '&#x20b1;'
+    ],
+
+    [
+        'Name' => 'Poland Zloty',
+        'Code' => 'PLN',
+        'unicodeHEX' => '&#x7a;&#x142;'
+    ],
+
+    [
+        'Name' => 'Qatar Riyal',
+        'Code' => 'QAR',
+        'unicodeHEX' => '&#xfdfc;'
+    ],
+
+    [
+        'Name' => 'Romania New Leu',
+        'Code' => 'RON',
+        'unicodeHEX' => '&#x6c;&#x65;&#x69;'
+    ],
+
+    [
+        'Name' => 'Russia Ruble',
+        'Code' => 'RUB',
+        'unicodeHEX' => '&#x20bd;'
+    ],
+
+    [
+        'Name' => 'Saint Helena Pound',
+        'Code' => 'SHP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Saudi Arabia Riyal',
+        'Code' => 'SAR',
+        'unicodeHEX' => '&#xfdfc;'
+    ],
+
+    [
+        'Name' => 'Serbia Dinar',
+        'Code' => 'RSD',
+        'unicodeHEX' => '&#x414;&#x438;&#x43d;&#x2e;'
+    ],
+
+    [
+        'Name' => 'Seychelles Rupee',
+        'Code' => 'SCR',
+        'unicodeHEX' => '&#x20a8;'
+    ],
+
+    [
+        'Name' => 'Singapore Dollar',
+        'Code' => 'SGD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Solomon Islands Dollar',
+        'Code' => 'SBD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Somalia Shilling',
+        'Code' => 'SOS',
+        'unicodeHEX' => '&#x53;'
+    ],
+
+    [
+        'Name' => 'South Africa Rand',
+        'Code' => 'ZAR',
+        'unicodeHEX' => '&#x52;'
+    ],
+
+    [
+        'Name' => 'Korea (South) Won',
+        'Code' => 'KRW',
+        'unicodeHEX' => '&#x20a9;'
+    ],
+
+    [
+        'Name' => 'Sri Lanka Rupee',
+        'Code' => 'LKR',
+        'unicodeHEX' => '&#x20a8;'
+    ],
+
+    [
+        'Name' => 'Sweden Krona',
+        'Code' => 'SEK',
+        'unicodeHEX' => '&#x6b;&#x72;'
+    ],
+
+    [
+        'Name' => 'Switzerland Franc',
+        'Code' => 'CHF',
+        'unicodeHEX' => '&#x43;&#x48;&#x46;'
+    ],
+
+    [
+        'Name' => 'Suriname Dollar',
+        'Code' => 'SRD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Syria Pound',
+        'Code' => 'SYP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'Taiwan New Dollar',
+        'Code' => 'TWD',
+        'unicodeHEX' => '&#x4e;&#x54;&#x24;'
+    ],
+
+    [
+        'Name' => 'Thailand Baht',
+        'Code' => 'THB',
+        'unicodeHEX' => '&#xe3f;'
+    ],
+
+    [
+        'Name' => 'Trinidad and Tobago Dollar',
+        'Code' => 'TTD',
+        'unicodeHEX' => '&#x54;&#x54;&#x24;'
+    ],
+
+    [
+        'Name' => 'Turkey Lira',
+        'Code' => 'TRY',
+        'unicodeHEX' => '&#x;'
+    ],
+
+    [
+        'Name' => 'Tuvalu Dollar',
+        'Code' => 'TVD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Ukraine Hryvnia',
+        'Code' => 'UAH',
+        'unicodeHEX' => '&#x20b4;'
+    ],
+
+    [
+        'Name' => 'United Kingdom Pound',
+        'Code' => 'GBP',
+        'unicodeHEX' => '&#xa3;'
+    ],
+
+    [
+        'Name' => 'United States Dollar',
+        'Code' => 'USD',
+        'unicodeHEX' => '&#x24;'
+    ],
+
+    [
+        'Name' => 'Uruguay Peso',
+        'Code' => 'UYU',
+        'unicodeHEX' => '&#x24;&#x55;'
+    ],
+
+    [
+        'Name' => 'Uzbekistan Som',
+        'Code' => 'UZS',
+        'unicodeHEX' => '&#x43b;&#x432;'
+    ],
+
+    [
+        'Name' => 'Venezuela Bolivar',
+        'Code' => 'VEF',
+        'unicodeHEX' => '&#x42;&#x73;'
+    ],
+
+    [
+        'Name' => 'Viet Nam Dong',
+        'Code' => 'VND',
+        'unicodeHEX' => '&#x20ab;'
+    ],
+
+    [
+        'Name' => 'Yemen Rial',
+        'Code' => 'YER',
+        'unicodeHEX' => '&#xfdfc;'
+    ],
+
+    [
+        'Name' => 'Zimbabwe Dollar',
+        'Code' => 'ZWD',
+        'unicodeHEX' => '&#x5a;&#x24;'
+    ]
+];
